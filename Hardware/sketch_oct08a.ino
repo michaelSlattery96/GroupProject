2c4354aebfed963f1467966bb08f6141fb7484ee
yradwan <31907260+yradwan@users.noreply.github.com>
2017-10-09 01:05:31 +0100

    Add files via upload
    This is a sketch that will put latitude and longitude readings from a 1sheeld onto our firebase database.

---
+++ Hardware/sketch_oct08a.ino
@@ -0,0 +1,24 @@
+#define CUSTOM_SETTINGS
+#define INCLUDE_INTERNET_SHIELD
+#define INCLUDE_GPS_SHIELD
+ 
+#include <OneSheeld.h>
+HttpRequest myRequest1("https://rd-year-group-project.firebaseio.com/latitude.json");
+HttpRequest myRequest2("https://rd-year-group-project.firebaseio.com/longitude.json");
+char myBuffer[10];
+ 
+void setup() {
+  // put your setup code here, to run once:
+  OneSheeld.begin();
+}
+ 
+void loop() {
+  // put your main code here, to run repeatedly:
+  float latitude = GPS.getLatitude();
+  float longitude = GPS.getLongitude();
+  myRequest1.addRawData(itoa(latitude,myBuffer,10));
+  myRequest2.addRawData(itoa(longitude,myBuffer,10));
+  Internet.performPut(myRequest1);
+  Internet.performPut(myRequest2);
+  OneSheeld.delay(60000*5);
+}
